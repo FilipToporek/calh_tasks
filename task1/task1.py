f9f939f18967305dc226a940b55b9b8009e3d60d
sava <savadoras@gmail.com>
2022-07-07 22:04:59 +0200

    add task1 solution

--- task1/task1.py
+++ task1/task1.py
@@ -3,6 +3,17 @@ import xml.etree.ElementTree as ET
 
 
 def task1_solution():
+    url = 'https://coding-academy.pl/all_customers'
+    response = requests.get(url)
+    root = ET.fromstring(response.content)
+
+    customer_numbers = list()
+    for element in root.findall('customer'):
+        customer_numbers.append(element.text)
+
+    with open(r'task1_solution.txt', 'w') as fp:
+        for number in customer_numbers:
+            fp.write(f"{number}\n")
     pass
 
 
